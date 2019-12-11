af351967d09cb82d5d680d9802a7a8f34f78b7d2
Ravindu <theravindu@gmail.com>
2019-12-11 03:11:30 -0500

    update

--- Exercise5/utils.py
+++ Exercise5/utils.py
@@ -8,28 +8,7 @@ from submission import SubmissionBase
 
 
 def trainLinearReg(linearRegCostFunction, X, y, lambda_=0.0, maxiter=200):
-    """
-    Trains linear regression using scipy's optimize.minimize.
-
-    Parameters
-    ----------
-    X : array_like
-        The dataset with shape (m x n+1). The bias term is assumed to be concatenated.
-
-    y : array_like
-        Function values at each datapoint. A vector of shape (m,).
-
-    lambda_ : float, optional
-        The regularization parameter.
-
-    maxiter : int, optional
-        Maximum number of iteration for the optimization algorithm.
-
-    Returns
-    -------
-    theta : array_like
-        The parameters for linear regression. This is a vector of shape (n+1,).
-    """
+   
     # Initialize Theta
     initial_theta = np.zeros(X.shape[1])
 
@@ -45,29 +24,7 @@ def trainLinearReg(linearRegCostFunction, X, y, lambda_=0.0, maxiter=200):
 
 
 def featureNormalize(X):
-    """
-    Normalizes the features in X returns a normalized version of X where the mean value of each
-    feature is 0 and the standard deviation is 1. This is often a good preprocessing step to do when
-    working with learning algorithms.
 
-    Parameters
-    ----------
-    X : array_like
-        An dataset which is a (m x n) matrix, where m is the number of examples,
-        and n is the number of dimensions for each example.
-
-    Returns
-    -------
-    X_norm : array_like
-        The normalized input dataset.
-
-    mu : array_like
-        A vector of size n corresponding to the mean for each dimension across all examples.
-
-    sigma : array_like
-        A vector of size n corresponding to the standard deviations for each dimension across
-        all examples.
-    """
     mu = np.mean(X, axis=0)
     X_norm = X - mu
 
@@ -77,34 +34,7 @@ def featureNormalize(X):
 
 
 def plotFit(polyFeatures, min_x, max_x, mu, sigma, theta, p):
-    """
-    Plots a learned polynomial regression fit over an existing figure.
-    Also works with linear regression.
-    Plots the learned polynomial fit with power p and feature normalization (mu, sigma).
 
-    Parameters
-    ----------
-    polyFeatures : func
-        A function which generators polynomial features from a single feature.
-
-    min_x : float
-        The minimum value for the feature.
-
-    max_x : float
-        The maximum value for the feature.
-
-    mu : float
-        The mean feature value over the training dataset.
-
-    sigma : float
-        The feature standard deviation of the training dataset.
-
-    theta : array_like
-        The parameters for the trained polynomial linear regression.
-
-    p : int
-        The polynomial order.
-    """
     # We plot a range slightly bigger than the min and max values to get
     # an idea of how the fit will vary outside the range of the data points
     x = np.arange(min_x - 15, max_x + 25, 0.05).reshape(-1, 1)
